76dda2116c9cd6cac32c5c65576485582dcd689f
nickrallison <nickrallison1@gmail.com>
2024-09-27 06:57:52 -0600

    fmt

--- src/main/scala/GPIO.scala
+++ src/main/scala/GPIO.scala
@@ -2,10 +2,12 @@
 // This code is licensed under the Apache Software License 2.0 (see LICENSE.MD)
 package tech.rocksavage.chiselware.GPIO
 
+import java.io.File
+import java.io.PrintWriter
+
+import _root_.circt.stage.ChiselStage
 import chisel3._
 import chisel3.util._
-import java.io.{File, PrintWriter}
-import _root_.circt.stage.ChiselStage
 
 class GPIO(p: BaseParams) extends Module {
   val io = IO(new Bundle {
@@ -31,12 +33,8 @@ class GPIO(p: BaseParams) extends Module {
   when(io.apb.PSEL && io.apb.PENABLE) {
     when(io.apb.PWRITE) { // Write Operation
       switch(io.apb.PADDR) { // Address for OUTPUT Register
-        is(0.U) {
-          MODE := io.apb.PWDATA
-        }
-        is(2.U) {
-          OUTPUT := io.apb.PWDATA
-        }
+        is(0.U)(MODE := io.apb.PWDATA)
+        is(2.U)(OUTPUT := io.apb.PWDATA)
         is(1.U) { // Address for DIRECTION Register
           DIRECTION := io.apb.PWDATA // Takes LSB of PWDATA
         }
@@ -44,44 +42,33 @@ class GPIO(p: BaseParams) extends Module {
       io.apb.PREADY := true.B
     }.otherwise { // Read Operation
       switch(io.apb.PADDR) {
-        is(0.U) {
-          io.apb.PRDATA := MODE
-        }
-        is(2.U) {
-          io.apb.PRDATA := OUTPUT
-        }
-        is(1.U) {
-          io.apb.PRDATA := DIRECTION
-        }
-        is(3.U) {
-          io.apb.PRDATA := INPUT
-        }
+        is(0.U)(io.apb.PRDATA := MODE)
+        is(2.U)(io.apb.PRDATA := OUTPUT)
+        is(1.U)(io.apb.PRDATA := DIRECTION)
+        is(3.U)(io.apb.PRDATA := INPUT)
       }
       io.apb.PREADY := true.B
     }
-  }.otherwise {
-    io.apb.PREADY := false.B
-  }
+  }.otherwise(io.apb.PREADY := false.B)
 
-  for (i <- 0 until p.dataWidth) {
-    when(MODE(i) === 1.U) { // AND each bit of DIRECTION to mask bits that are not set as OUTPUT in GPIO_O
-      GPIO_O_VEC(p.dataWidth - i - 1) := OUTPUT(i) & DIRECTION(i)
-      GPIO_OE_VEC(p.dataWidth - i - 1) := DIRECTION(i)
-    }.otherwise {
-      GPIO_O_VEC(p.dataWidth - i - 1) := 0.U
-      GPIO_OE_VEC(p.dataWidth - i - 1) := ~OUTPUT(i) & DIRECTION(i)
-    }
-    // Pads Implementation
-    // io.pins.pads(i) := Mux(GPIO_OE(i) === 1.U, GPIO_O(i), 0.U)
-    // GPIO_I(i) := Mux(GPIO_OE(i) === 0.U, io.pins.pads(i), 0.U)
+  for (i <- 0 until p.dataWidth) when(MODE(i) === 1.U) { // AND each bit of DIRECTION to mask bits that are not set as OUTPUT in GPIO_O
+    GPIO_O_VEC(p.dataWidth - i - 1) := OUTPUT(i) & DIRECTION(i)
+    GPIO_OE_VEC(p.dataWidth - i - 1) := DIRECTION(i)
+  }.otherwise {
+    GPIO_O_VEC(p.dataWidth - i - 1) := 0.U
+    GPIO_OE_VEC(p.dataWidth - i - 1) := ~OUTPUT(i) & DIRECTION(i)
   }
+  // Pads Implementation
+  // io.pins.pads(i) := Mux(GPIO_OE(i) === 1.U, GPIO_O(i), 0.U)
+  // GPIO_I(i) := Mux(GPIO_OE(i) === 0.U, io.pins.pads(i), 0.U)
   INPUT := io.pins.GPIO_I
   io.pins.GPIO_O := Cat(GPIO_O_VEC)
   io.pins.GPIO_OE := Cat(GPIO_OE_VEC)
 
   // Handle invalid address case
   when(
-    io.apb.PADDR =/= 0.U && io.apb.PADDR =/= 1.U && io.apb.PADDR =/= 2.U && io.apb.PADDR =/= 3.U
+    io.apb.PADDR =/= 0.U && io.apb.PADDR =/= 1.U && io.apb.PADDR =/= 2.U &&
+      io.apb.PADDR =/= 3.U,
   ) {
     io.apb.PSLVERR := true.B // Set error signal
   }.otherwise {
