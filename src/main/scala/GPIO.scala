3eef658c4790fa4c28ddeeb25439c998ffb41b33
nickrallison <nickrallison1@gmail.com>
2024-09-27 07:37:57 -0600

    added register decode

--- src/main/scala/GPIO.scala
+++ src/main/scala/GPIO.scala
@@ -19,11 +19,7 @@ class GPIO(p: BaseParams) extends Module {
     }
   })
 
-  // User Accessible Control Registers
-  val DIRECTION = RegInit(0.U(p.dataWidth.W)) // RW
-  val OUTPUT = RegInit(0.U(p.dataWidth.W)) // RW
-  val INPUT = RegInit(0.U(p.dataWidth.W)) // RO
-  val MODE = RegInit(0.U(p.dataWidth.W)) // RW
+  val regs = new GPIORegs(p)
 
   // Intermediary Signals
   val GPIO_O_VEC = Wire(Vec(p.dataWidth, UInt(1.W)))
@@ -32,36 +28,38 @@ class GPIO(p: BaseParams) extends Module {
   io.apb.PRDATA := 0.U // Init value
   when(io.apb.PSEL && io.apb.PENABLE) {
     when(io.apb.PWRITE) { // Write Operation
-      switch(io.apb.PADDR) { // Address for OUTPUT Register
-        is(0.U)(MODE := io.apb.PWDATA)
-        is(2.U)(OUTPUT := io.apb.PWDATA)
-        is(1.U) { // Address for DIRECTION Register
-          DIRECTION := io.apb.PWDATA // Takes LSB of PWDATA
-        }
-      }
+      // switch(io.apb.PADDR) { // Address for OUTPUT Register
+      //   is(0.U)(regs.MODE := io.apb.PWDATA)
+      //   is(2.U)(regs.OUTPUT := io.apb.PWDATA)
+      //   is(1.U) { // Address for DIRECTION Register
+      //     regs.DIRECTION := io.apb.PWDATA // Takes LSB of PWDATA
+      //   }
+      // }
+      registerDecodeWrite(io.apb.PADDR)
       io.apb.PREADY := true.B
     }.otherwise { // Read Operation
-      switch(io.apb.PADDR) {
-        is(0.U)(io.apb.PRDATA := MODE)
-        is(2.U)(io.apb.PRDATA := OUTPUT)
-        is(1.U)(io.apb.PRDATA := DIRECTION)
-        is(3.U)(io.apb.PRDATA := INPUT)
-      }
+      // switch(io.apb.PADDR) {
+      //   is(0.U)(io.apb.PRDATA := regs.MODE)
+      //   is(2.U)(io.apb.PRDATA := regs.OUTPUT)
+      //   is(1.U)(io.apb.PRDATA := regs.DIRECTION)
+      //   is(3.U)(io.apb.PRDATA := regs.INPUT)
+      // }
+      registerDecodeRead(io.apb.PADDR)
       io.apb.PREADY := true.B
     }
   }.otherwise(io.apb.PREADY := false.B)
 
-  for (i <- 0 until p.dataWidth) when(MODE(i) === 1.U) { // AND each bit of DIRECTION to mask bits that are not set as OUTPUT in GPIO_O
-    GPIO_O_VEC(p.dataWidth - i - 1) := OUTPUT(i) & DIRECTION(i)
-    GPIO_OE_VEC(p.dataWidth - i - 1) := DIRECTION(i)
+  for (i <- 0 until p.dataWidth) when(regs.MODE(i) === 1.U) { // AND each bit of DIRECTION to mask bits that are not set as OUTPUT in GPIO_O
+    GPIO_O_VEC(p.dataWidth - i - 1) := regs.OUTPUT(i) & regs.DIRECTION(i)
+    GPIO_OE_VEC(p.dataWidth - i - 1) := regs.DIRECTION(i)
   }.otherwise {
     GPIO_O_VEC(p.dataWidth - i - 1) := 0.U
-    GPIO_OE_VEC(p.dataWidth - i - 1) := ~OUTPUT(i) & DIRECTION(i)
+    GPIO_OE_VEC(p.dataWidth - i - 1) := ~regs.OUTPUT(i) & regs.DIRECTION(i)
   }
   // Pads Implementation
   // io.pins.pads(i) := Mux(GPIO_OE(i) === 1.U, GPIO_O(i), 0.U)
   // GPIO_I(i) := Mux(GPIO_OE(i) === 0.U, io.pins.pads(i), 0.U)
-  INPUT := io.pins.GPIO_I
+  regs.INPUT := io.pins.GPIO_I
   io.pins.GPIO_O := Cat(GPIO_O_VEC)
   io.pins.GPIO_OE := Cat(GPIO_OE_VEC)
 
@@ -75,4 +73,125 @@ class GPIO(p: BaseParams) extends Module {
     io.apb.PSLVERR := false.B // Clear error signal if valid
   }
 
+  // function to take addr and data from APB and write to gpio register space and or child modules if applicable
+  def registerDecodeWrite(addr: UInt): Unit = {
+    val DIRECTION_SIZE = p.dataWidth
+    val OUTPUT_SIZE = p.dataWidth
+    val INPUT_SIZE = p.dataWidth
+    val MODE_SIZE = p.dataWidth
+
+    val ATOMIC_OPERATION_SIZE = 4
+    val ATOMIC_MASK_SIZE = p.dataWidth
+    val ATOPIC_SET_SIZE = 1
+
+    val DIRECTION_ADDR = 0.U
+    val DIRECTION_ADDR_MAX = DIRECTION_ADDR + DIRECTION_SIZE.U - 1.U
+
+    val OUTPUT_ADDR = DIRECTION_ADDR + DIRECTION_SIZE.U
+    val OUTPUT_ADDR_MAX = OUTPUT_ADDR + OUTPUT_SIZE.U - 1.U
+
+    val INPUT_ADDR = OUTPUT_ADDR + OUTPUT_SIZE.U
+    val INPUT_ADDR_MAX = INPUT_ADDR + INPUT_SIZE.U - 1.U
+
+    val MODE_ADDR = INPUT_ADDR + INPUT_SIZE.U
+    val MODE_ADDR_MAX = MODE_ADDR + MODE_SIZE.U - 1.U
+
+    val ATOMIC_OPERATION_ADDR = MODE_ADDR + MODE_SIZE.U
+    val ATOMIC_OPERATION_ADDR_MAX = ATOMIC_OPERATION_ADDR +
+      ATOMIC_OPERATION_SIZE.U - 1.U
+
+    val ATOMIC_MASK_ADDR = ATOMIC_OPERATION_ADDR + ATOMIC_OPERATION_SIZE.U
+    val ATOMIC_MASK_ADDR_MAX = ATOMIC_MASK_ADDR + ATOMIC_MASK_SIZE.U - 1.U
+
+    val ATOMIC_SET_ADDR = ATOMIC_MASK_ADDR + ATOMIC_MASK_SIZE.U
+    val ATOMIC_SET_ADDR_MAX = ATOMIC_SET_ADDR + ATOPIC_SET_SIZE.U - 1.U
+    when(addr >= DIRECTION_ADDR && addr <= DIRECTION_ADDR_MAX) {
+      regs.DIRECTION := io.apb.PWDATA(DIRECTION_SIZE - 1, 0)
+    }
+    when(addr >= OUTPUT_ADDR && addr <= OUTPUT_ADDR_MAX) {
+      regs.OUTPUT := io.apb.PWDATA(OUTPUT_SIZE - 1, 0)
+    }
+    when(addr >= MODE_ADDR && addr <= MODE_ADDR_MAX) {
+      regs.MODE := io.apb.PWDATA(MODE_SIZE - 1, 0)
+    }
+    when(addr >= ATOMIC_OPERATION_ADDR && addr <= ATOMIC_OPERATION_ADDR_MAX) {
+      regs.ATOMIC_OPERATION := io.apb.PWDATA(ATOMIC_OPERATION_SIZE - 1, 0)
+    }
+    when(addr >= ATOMIC_MASK_ADDR && addr <= ATOMIC_MASK_ADDR_MAX) {
+      regs.ATOMIC_MASK := io.apb.PWDATA(ATOMIC_MASK_SIZE - 1, 0)
+    }
+    when(addr >= ATOMIC_SET_ADDR && addr <= ATOMIC_SET_ADDR_MAX) {
+      regs.ATOMIC_SET := io.apb.PWDATA(ATOPIC_SET_SIZE - 1, 0)
+    }
+  }
+
+  // function to take addr and data from APB and write to gpio register space and or child modules if applicable
+  def registerDecodeRead(addr: UInt): Unit = {
+    val DIRECTION_SIZE = p.dataWidth
+    val OUTPUT_SIZE = p.dataWidth
+    val INPUT_SIZE = p.dataWidth
+    val MODE_SIZE = p.dataWidth
+
+    val ATOMIC_OPERATION_SIZE = 4
+    val ATOMIC_MASK_SIZE = p.dataWidth
+    val ATOPIC_SET_SIZE = 1
+
+    val DIRECTION_ADDR = 0.U
+    val DIRECTION_ADDR_MAX = DIRECTION_ADDR + DIRECTION_SIZE.U - 1.U
+
+    val OUTPUT_ADDR = DIRECTION_ADDR + DIRECTION_SIZE.U
+    val OUTPUT_ADDR_MAX = OUTPUT_ADDR + OUTPUT_SIZE.U - 1.U
+
+    val INPUT_ADDR = OUTPUT_ADDR + OUTPUT_SIZE.U
+    val INPUT_ADDR_MAX = INPUT_ADDR + INPUT_SIZE.U - 1.U
+
+    val MODE_ADDR = INPUT_ADDR + INPUT_SIZE.U
+    val MODE_ADDR_MAX = MODE_ADDR + MODE_SIZE.U - 1.U
+
+    val ATOMIC_OPERATION_ADDR = MODE_ADDR + MODE_SIZE.U
+    val ATOMIC_OPERATION_ADDR_MAX = ATOMIC_OPERATION_ADDR +
+      ATOMIC_OPERATION_SIZE.U - 1.U
+
+    val ATOMIC_MASK_ADDR = ATOMIC_OPERATION_ADDR + ATOMIC_OPERATION_SIZE.U
+    val ATOMIC_MASK_ADDR_MAX = ATOMIC_MASK_ADDR + ATOMIC_MASK_SIZE.U - 1.U
+
+    val ATOMIC_SET_ADDR = ATOMIC_MASK_ADDR + ATOMIC_MASK_SIZE.U
+    val ATOMIC_SET_ADDR_MAX = ATOMIC_SET_ADDR + ATOPIC_SET_SIZE.U - 1.U
+
+    when(addr >= DIRECTION_ADDR && addr <= DIRECTION_ADDR_MAX) {
+      io.apb.PRDATA := regs.DIRECTION
+    }
+    when(addr >= OUTPUT_ADDR && addr <= OUTPUT_ADDR_MAX) {
+      io.apb.PRDATA := regs.OUTPUT
+    }
+    when(addr >= INPUT_ADDR && addr <= INPUT_ADDR_MAX) {
+      io.apb.PRDATA := regs.INPUT
+    }
+    when(addr >= MODE_ADDR && addr <= MODE_ADDR_MAX) {
+      io.apb.PRDATA := regs.MODE
+    }
+    when(addr >= ATOMIC_OPERATION_ADDR && addr <= ATOMIC_OPERATION_ADDR_MAX) {
+      io.apb.PRDATA := regs.ATOMIC_OPERATION.asUInt
+    }
+    when(addr >= ATOMIC_MASK_ADDR && addr <= ATOMIC_MASK_ADDR_MAX) {
+      io.apb.PRDATA := regs.ATOMIC_MASK
+    }
+    when(addr >= ATOMIC_SET_ADDR && addr <= ATOMIC_SET_ADDR_MAX) {
+      io.apb.PRDATA := regs.ATOMIC_SET
+    }
+
+  }
+}
+
+class GPIORegs(p: BaseParams) extends Bundle {
+  // User Accessible Control Registers
+  val DIRECTION = RegInit(0.U(p.dataWidth.W)) // RW
+  val OUTPUT = RegInit(0.U(p.dataWidth.W)) // RW
+  val INPUT = RegInit(0.U(p.dataWidth.W)) // RO
+  val MODE = RegInit(0.U(p.dataWidth.W)) // RW
+
+  // Atomic Operations
+  val ATOMIC_OPERATION = RegInit(0.U(4.W))
+  val ATOMIC_MASK = RegInit(0.U(p.dataWidth.W))
+  val ATOMIC_SET = RegInit(0.U(1.W))
 }
--- src/test/scala/GPIOTest.scala
+++ src/test/scala/GPIOTest.scala
@@ -1,19 +1,25 @@
 package tech.rocksavage.chiselware.GPIO
 
+import java.io.BufferedWriter
+import java.io.File
+import java.io.FileWriter
+import java.io.PrintWriter
+
+import scala.collection.immutable.ListMap
+import scala.collection.mutable.Stack
+import scala.math.pow
+import scala.util.Random
+
+import org.scalatest.Assertions._
+import org.scalatest._
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
 import chisel3._
 import chisel3.util._
 import chiseltest._
 import chiseltest.coverage._
-import org.scalatest._
-import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should.Matchers
-import org.scalatest.Assertions._
 import firrtl2.options.TargetDirAnnotation
-import scala.util.Random
-import scala.math.pow
-import scala.collection.mutable.Stack
-import scala.collection.immutable.ListMap
-import java.io.{File, FileWriter, PrintWriter, BufferedWriter}
 
 class GPIOTest extends AnyFlatSpec with ChiselScalatestTester with Matchers {
 
@@ -21,76 +27,68 @@ class GPIOTest extends AnyFlatSpec with ChiselScalatestTester with Matchers {
     dataWidth = 32,
     coverage = false,
     physicalPorts = 1,
-    virtualPorts = 1
+    virtualPorts = 1,
   )
-  "GPIO" should "work" in {
+  "GPIO" should "work" in test(new GPIO(myParams)) { dut =>
+    def writeAPB(addr: UInt, data: UInt): Unit = {
+      dut.io.apb.PSEL.poke(1) // Select GPIO Slave
+      dut.clock.step(1)
+      dut.io.apb.PENABLE.poke(1) // Enable APB
+      dut.io.apb.PWRITE.poke(1) // Write mode
+      dut.io.apb.PADDR.poke(addr)
+      dut.io.apb.PWDATA.poke(data)
+      dut.clock.step(1) // TODO: Should test this with variable wait states
+      dut.io.apb.PSEL.poke(0) // Select GPIO Slave
+      dut.io.apb.PENABLE.poke(0) // Enable APB
+      dut.clock.step(2)
+    }
 
-    test(new GPIO(myParams)) { dut =>
-      def writeAPB(addr: UInt, data: UInt): Unit = {
-        dut.io.apb.PSEL.poke(1) // Select GPIO Slave
-        dut.clock.step(1)
-        dut.io.apb.PENABLE.poke(1) // Enable APB
-        dut.io.apb.PWRITE.poke(1) // Write mode
-        dut.io.apb.PADDR.poke(addr)
-        dut.io.apb.PWDATA.poke(data)
-        dut.clock.step(1) // TODO: Should test this with variable wait states
-        dut.io.apb.PSEL.poke(0) // Select GPIO Slave
-        dut.io.apb.PENABLE.poke(0) // Enable APB
-        dut.clock.step(2)
-      }
+    def readAPB(addr: UInt): BigInt = {
+      dut.io.apb.PSEL.poke(1) // Select APB
+      dut.clock.step(1)
+      dut.io.apb.PENABLE.poke(1) // Enable APB
+      dut.io.apb.PWRITE.poke(0) // Read mode
+      dut.io.apb.PADDR.poke(addr)
+      dut.clock.step(1)
+      val readValue = dut.io.apb.PRDATA.peekInt() // Return read data
+      dut.clock.step(1) // Step for the read operation
+      dut.io.apb.PSEL.poke(0) // Select GPIO Slave
+      dut.io.apb.PENABLE.poke(0) // Enable APB
+      dut.clock.step(2)
+      readValue
+    }
 
-      def readAPB(addr: UInt): BigInt = {
-        dut.io.apb.PSEL.poke(1) // Select APB
-        dut.clock.step(1)
-        dut.io.apb.PENABLE.poke(1) // Enable APB
-        dut.io.apb.PWRITE.poke(0) // Read mode
-        dut.io.apb.PADDR.poke(addr)
-        dut.clock.step(1)
-        val readValue = dut.io.apb.PRDATA.peekInt() // Return read data
-        dut.clock.step(1) // Step for the read operation
-        dut.io.apb.PSEL.poke(0) // Select GPIO Slave
-        dut.io.apb.PENABLE.poke(0) // Enable APB
-        dut.clock.step(2)
-        readValue
-      }
+    // APB Tests
+    // Test 1: Write to OUTPUT register and read back
+    println("Test 1: Write to OUTPUT register")
+    writeAPB(2.U, 30.U) // Write 0b1010 to OUTPUT
+    val outputData = readAPB(2.U)
+    println(s"Output Register Read: ${outputData.toString()}") // Should be 10
+    require(outputData == 30)
 
-      // APB Tests
-      // Test 1: Write to OUTPUT register and read back
-      println("Test 1: Write to OUTPUT register")
-      writeAPB(2.U, 30.U) // Write 0b1010 to OUTPUT
-      val outputData = readAPB(2.U)
-      println(
-        s"Output Register Read: ${outputData.toString()}"
-      ) // Should be 10
-      require(outputData == 30)
+    // Test 2: Write to DIRECTION register and read back
+    println("Test 2: Write to DIRECTION register")
+    writeAPB(1.U, 31.U) // Set DIRECTION to all outputs
+    val gpioO = readAPB(1.U) // Read DIRECTION Register
+    println(s"GPIO_O after DIRECTION set: ${gpioO.toString()}")
+    require(gpioO == 31)
 
-      // Test 2: Write to DIRECTION register and read back
-      println("Test 2: Write to DIRECTION register")
-      writeAPB(1.U, 31.U) // Set DIRECTION to all outputs
-      val gpioO = readAPB(1.U) // Read DIRECTION Register
-      println(
-        s"GPIO_O after DIRECTION set: ${gpioO.toString()}"
-      )
-      require(gpioO == 31)
+    // Test 3: Write to MODE register and read back
+    println("Test 3: Write to MODE register")
+    writeAPB(0.U, 30.U) // Set DIRECTION to all outputs
+    val MODE = readAPB(0.U) // Read DIRECTION Register
+    println(s"After MODE set: ${MODE.toString()}")
+    require(MODE == 30)
 
-      // Test 3: Write to MODE register and read back
-      println("Test 3: Write to MODE register")
-      writeAPB(0.U, 30.U) // Set DIRECTION to all outputs
-      val MODE = readAPB(0.U) // Read DIRECTION Register
-      println(
-        s"After MODE set: ${MODE.toString()}"
-      )
-      require(MODE == 30)
+    // Test PPL and Open-Drain Mode:
+    val PPL = dut.io.pins.GPIO_O.peekInt()
+    println(s"Output Reg: ${outputData.toString(2)}")
+    println(s"Direction Reg: ${gpioO.toString(2)}")
+    println(s"Mode Reg: ${MODE.toString(2)}")
+    println(s"GPIO_O: ${PPL.toString(2)}")
+    println(s"GPIO_OE: ${dut.io.pins.GPIO_OE.peekInt().toString(2)}")
 
-      // Test PPL and Open-Drain Mode:
-      val PPL = dut.io.pins.GPIO_O.peekInt()
-      println(s"Output Reg: ${outputData.toString(2)}")
-      println(s"Direction Reg: ${gpioO.toString(2)}")
-      println(s"Mode Reg: ${MODE.toString(2)}")
-      println(s"GPIO_O: ${PPL.toString(2)}")
-      println(s"GPIO_OE: ${dut.io.pins.GPIO_OE.peekInt().toString(2)}")
-
-      /*
+    /*
       val PPL: Array[BigInt] = new Array[BigInt](myParams.dataWidth)
       for (i <- 0 until myParams.dataWidth) {
         PPL(i) = dut.io.pins.GPIO_O(i).peekInt()
@@ -98,24 +96,21 @@ class GPIOTest extends AnyFlatSpec with ChiselScalatestTester with Matchers {
           s"${PPL(i)}"
         )
       }
-       */
+     */
 
-      // Test 4: Test reading of INPUT register
-      println("Test 4: INPUT Register")
-      dut.io.pins.GPIO_I.poke(16.U)
-      val gpioI = readAPB(3.U) // Read DIRECTION Register
-      println(
-        s"GPIO_I after Input set: ${gpioI.toString()}"
-      )
-      require(gpioI == 16)
+    // Test 4: Test reading of INPUT register
+    println("Test 4: INPUT Register")
+    dut.io.pins.GPIO_I.poke(16.U)
+    val gpioI = readAPB(3.U) // Read DIRECTION Register
+    println(s"GPIO_I after Input set: ${gpioI.toString()}")
+    require(gpioI == 16)
 
-      // Test 5: Invalid address handling
-      println("Test 5: Invalid address handling")
-      dut.io.apb.PADDR.poke(10.U) // Invalid address
-      dut.clock.step(1)
-      require(dut.io.apb.PSLVERR.peekInt() == 1) // Should set error signal
-
-    }
+    // Test 5: Invalid address handling
+    println("Test 5: Invalid address handling")
+    dut.io.apb.PADDR.poke(10.U) // Invalid address
+    dut.clock.step(1)
+    require(dut.io.apb.PSLVERR.peekInt() == 1) // Should set error signal
 
   }
+
 }
