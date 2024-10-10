6dd891ba6b928614ce4f44f18746866c5a0bf5df
nickrallison <nickrallison1@gmail.com>
2024-10-09 19:28:14 -0600

    removed timing rpt

--- synth/sta.sh
+++ synth/sta.sh
@@ -3,5 +3,8 @@ cd generated
 testCase=GPIO
 sta -no_init -no_splash -exit ../synth/GPIO.sta.tcl | tee ./timing.rpt
 timing=`grep slack ./timing.rpt`
+if [ -e ../timing_slack.rpt ]; then 
+    rm -f ../timing_slack.rpt
+fi
 echo -e "$testCase = \t $timing" >> ./timing_slack.rpt 
 cd ../synth 
